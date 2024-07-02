e615a44054a2172ccd03c7b7a7c16770bc2b4e61
Ologunjae <105816803+Ologunjae@users.noreply.github.com>
2024-07-02 13:37:58 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
 import requests
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+load_dotenv()
+
+@app.route('/api/hello', methods=['GET'])
 def hello():
     visitor_name = request.args.get('visitor_name', 'Guest')
     ip = request.remote_addr
@@ -17,6 +21,20 @@ def hello():
     }
     return jsonify(response)
 
+@app.route('/', methods=['GET'])
+def hello2():
+    visitor_name = request.args.get('visitor_name', 'Guest')
+    ip = request.remote_addr
+    location = get_location(ip)
+    temperature = get_temperature(location)
+    greeting = f"Hello, {visitor_name}!, The intensity is {temperature} degrees Celsius in {location}."
+    response = {
+        "client_ip": ip,
+        "location": location,
+        "greeting": greeting
+    }
+    return jsonify(response)
+    
 def get_location(ip):
     response = requests.get(f"http://ip-api.com/json/{ip}")
     data = response.json()
@@ -24,11 +42,15 @@ def get_location(ip):
     return location
 
 def get_temperature(location):
-    api_key = '133b79edd1fa5214bfdcb3de31bbd027'
+    api_key = os.getenv('api_key')
     response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric")
     data = response.json()
     temperature = data.get('main', {}).get('temp')
     return temperature
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
